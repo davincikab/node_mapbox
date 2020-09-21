191368fb9d350fc14d54b9d6eca151abc51843f2
davincikab <davidnganganjeri079@gmail.com>
2020-09-21 07:41:04 +0300

    updated database credentials

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user:process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: 'localhost',
+  user:'root',
+  password: 'root',
+  database:'density' 
 });
 
 connection.connect();
--- models/userModel.js
+++ models/userModel.js
@@ -14,7 +14,8 @@ var Person = function(person) {
 	this.ID_ESC = person.id_esc;
 	this.GEN = person.gen;
 	this.PROFESSION = person.profession;
-	this.PIC_PROFILE = person.pic_profile;
+    this.PIC_PROFILE = person.pic_profile;
+    this.AUTHORIZED = person.authorized;
 }
 
 Person.getAllPersons = function(result) {
@@ -32,7 +33,7 @@ Person.getAllPersons = function(result) {
 }
 
 Person.getNewPersons = function(lastId, result) {
-	sql.query("SELECT ID_CAN, ALIAS, COUNTRY, STATE, CITY, LONGITUDE, ALTITUDE, DOB, PROFESSION, PIC_PROFILE from location_data WHERE ID_CAN > ? ", [lastId] , function (err, res) {
+	sql.query("SELECT ID_CAN, ALIAS, COUNTRY, STATE, CITY, LONGITUDE, ALTITUDE, DOB, PROFESSION, PIC_PROFILE from location_data WHERE ID_CAN > ? AND AUTHORIZED = ?", [lastId, 'Y'] , function (err, res) {
 
         if(err) {
             console.log("error: ", err);
